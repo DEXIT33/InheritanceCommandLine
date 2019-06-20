8096da00d0cbf6955356f1aae867ac914ff7fb89
David Domkář <domkard@gmail.com>
2019-06-20 21:23:13 +0200

    Basic inheritance

--- InheritanceCommandLine/Program.cs
+++ InheritanceCommandLine/Program.cs
@@ -6,7 +6,13 @@ namespace InheritanceCommandLine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rectangle = new Rectangle();
+            rectangle.a = 5;
+            rectangle.b = 10;
+
+            Square square = new Square();
+            square.a = 5;
+            square.b = 10;
         }
     }
 }
---
+++ InheritanceCommandLine/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceCommandLine
+{
+    class Rectangle
+    {
+        public int a;
+        public int b;
+    }
+}
